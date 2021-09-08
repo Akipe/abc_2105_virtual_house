5c8a3e808e59904a717b31a4e743d84c8092348c
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-09-08 11:27:59 +0200

    Add first working V1 garage door

--- src/virtualhouse_v1/App.java
+++ src/virtualhouse_v1/App.java
@@ -1,9 +1,20 @@
 package virtualhouse_v1;
 
+import virtualhouse_v1.lib.Virtualhouse;
 import virtualhouse_v1.lib.door.GarageDoor;
 
 public class App {
     public static void main(String[] args) {
-        
+        Virtualhouse    a_virtual_house;
+        GarageDoor      a_garage_door;
+
+        a_virtual_house = new Virtualhouse();
+
+        a_garage_door = new GarageDoor(a_virtual_house);
+
+        a_garage_door.close();
+        a_garage_door.lock();
+
+        System.out.println(a_garage_door.getIsLock());;
     }
 }
